423948df3f593c5fb7cf9892b4573ecbb652c588
raghavmanchanda2 <raghavmanchanda2@gmail.com>
2023-09-24 01:54:20 -0400

    map validations

--- src/business/ExecuteMapsCommands.java
+++ src/business/ExecuteMapsCommands.java
@@ -2,6 +2,10 @@ package business;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import Constants.ProjectConfig;
 import GamePhase.MapPhaseState;
 import model.Continent;
@@ -49,15 +53,62 @@ public class ExecuteMapsCommands {
 
 		return null;
 	}
-	
+
 	public ResponseWrapper saveMap(String mapFileName) {
-		
+
 		return null;
 	}
-	public ResponseWrapper validateMap() {
-		
-		return null;
-	}
+
+	public ResponseWrapper validateMap(MapModel mapObject) {
+
+		Set<Continent> continents = new HashSet<Continent>(mapObject.getContinents());
+		Set<Country> countries = new HashSet<Country>(mapObject.getCountries());
+
+		Boolean countryBorderRelevantData = countries.stream()
+				.filter((country) -> !mapObject.getBorders().containsKey(country)).collect(Collectors.toList())
+				.size() == 0 ? true : false;
+		Boolean countryContinentNotExists = mapObject.getCountries().stream()
+				.anyMatch((country) -> country.getContinent().getContinentId() == null
+						|| country.getContinent().getContientValue() == null
+						|| "".equals(country.getContinent().getContinentId())
+						|| "".equals(country.getContinent().getContientValue()));
+		Boolean countryContinentExistsInContinentsList = mapObject.getCountries().stream()
+				.allMatch((country) -> continents.contains(country.getContinent().getContientValue()));
+		Boolean countryBorderNotExists = mapObject.getBorders().entrySet().stream()
+				.anyMatch(borderMap -> borderMap.getValue().size() == 0) ? true : false;
+
+		if ("".equals(mapObject.getMapName()) || mapObject.getMapName() == null || mapObject.getContinents().size() == 0
+				|| mapObject.getCountries().size() == 0 || mapObject.getContinentCountries().size() == 0
+				|| mapObject.getBorders().size() == 0) {
+
+			return new ResponseWrapper(404, "Map is not created Properly");
+
+		} else if (continents.size() != mapObject.getContinents().size()
+				|| countries.size() != mapObject.getContinents().size()) {
+			return new ResponseWrapper(404, "Duplicate Continent or Country Found in map");
+
+		} else if (Boolean.TRUE.equals(countryContinentNotExists)) {
+			return new ResponseWrapper(404, "Country Continent is missing");
+
+		} else if (countries.size() < 2) {
+			return new ResponseWrapper(404, " Countries Should be Atleast 2 in map ");
+
+		} else if (Boolean.FALSE.equals(countryBorderRelevantData)) {
+			return new ResponseWrapper(404,
+					" Border Data for Countries is not consistent with Countries that are added ");
+
+		} else if (Boolean.FALSE.equals(countryContinentExistsInContinentsList)) {
+			return new ResponseWrapper(404, " Countries Continents Mismatch ");
+
+		} else if (Boolean.FALSE.equals(countryContinentExistsInContinentsList)) {
+			return new ResponseWrapper(404, " Countries Continents Mismatch ");
+
+		} else if (Boolean.TRUE.equals(countryBorderNotExists)) {
+			return new ResponseWrapper(404, " Countries Border Missing ");
+		}
+
+		return new ResponseWrapper(200, " VALIDATION SUCCESSFUL ");	
+		}
 
 	public ResponseWrapper editOrCreateMap(String mapFileName) {
 
@@ -90,11 +141,6 @@ public class ExecuteMapsCommands {
 	}
 
 	public void showMap(String mapFileName) {
-
-	}
-
-	public ResponseWrapper validateMap(String mapFileName) {
-		return null;
 
 	}
 }
