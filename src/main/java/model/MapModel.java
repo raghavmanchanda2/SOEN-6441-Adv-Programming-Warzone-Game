9a6fb1ec9dc60f8aae757d21216c6470693c9b3b
Ishaan Bajaj <ishaanbajaj12@gmail.com>
2023-10-04 15:01:46 -0400

    Bug Fixes

--- src/main/java/model/MapModel.java
+++ src/main/java/model/MapModel.java
@@ -20,6 +20,10 @@ public class MapModel {
 	private String mapName;
 	private List<Continent> continents;
 	private List<Country> countries;
+
+	Map<String,Country> d_CountriesMap = new HashMap<String,Country>();
+
+
 	private Map<Continent,List<Country>> continentCountries;
 	private Map<Country,List<Country>> borders;
 
@@ -29,7 +33,7 @@ public class MapModel {
 	private ConsoleWriter d_consoleWriter;
 
 	private HashMap<String, Continent> d_Continents = new HashMap<>();
-	private HashMap<String, Country> d_Countries = new HashMap<>();
+	HashMap<String, Country> countryHashMap = new HashMap<>();
 
 	private HashMap<String, Player> d_Players = new HashMap<>();
 
@@ -206,13 +210,7 @@ public class MapModel {
 		return d_Continents;
 	}
 
-	public Country getCountry(String p_Id) {
-		return d_Countries.get(p_Id);
-	}
 
-	public HashMap<String, Country> getD_Countries() {
-		return d_Countries;
-	}
 
 	public Player getPlayer(String p_Id) {
 		return d_Players.get(p_Id);
@@ -222,55 +220,52 @@ public class MapModel {
 		return d_Players;
 	}
 
+
+	public Country getCountry(String p_Id) {
+
+		for (Country country : countries) {
+			String uniqueId = country.getCountryId();
+			countryHashMap.put(uniqueId, country);
+		}
+		return countryHashMap.get(p_Id);
+	}
+
+	public String getCountriesList(List<Country> countriesList) {
+		String l_countList = "";
+		for (Country country : countriesList) {
+			l_countList += country.getCountryId() + "-";
+		}
+		return l_countList.length() > 0 ? l_countList.substring(0, l_countList.length() - 1) : "";
+	}
+
 	public void showMap() {
 
-		System.out.format("\n Map Details are : \n");
-		System.out.format("\n Continents of Map are : \n");
-		System.out.format("+------------------+%n");
-		System.out.format("| Continent's Name |%n");
-		System.out.format("+------------------+%n");
+		d_logger.setLogMessage(" Continents: ");
+		d_logger.setLogMessage("********************");
+		d_logger.setLogMessage("  Continent's Name  ");
+		d_logger.setLogMessage("********************");
 
 		getContinents().stream().forEach((continent) -> {
-			String l_table = "|%-20s|%n";
+			String l_table = "- %-20s%n";
 			System.out.format(l_table, continent.getContinentId());
 		});
 
-		System.out.format("+------------------+%n");
+		d_logger.setLogMessage("********************");
 
 
-
-		// Showing Countries in the Continent and their details
+		/////////////
 		d_logger.setLogMessage("\nCountries:");
 
-		Iterator<Map.Entry<String, Continent>> l_IteratorForContinent = d_MapModel.getD_Continents().entrySet()
-				.iterator();
-
-		String l_tablePattern = "- %-23s- %-18s- %-60s%n";
-
-		System.out.format(
-				"****************************************************************************************************%n");
-		System.out.format(
-				"        Country   !     Continent    !     Neighbours                                  %n");
-		System.out.format(
-				"****************************************************************************************************%n");
-
-
-		while (l_IteratorForContinent.hasNext()) {
-			Map.Entry<String, Continent> l_ContinentMap = (Map.Entry<String, Continent>) l_IteratorForContinent.next();
-			String l_ContinentId = (String) l_ContinentMap.getKey();
-			Continent l_Continent = d_MapModel.getD_Continents().get(l_ContinentId);
-			Iterator<Country> l_ListIterator = l_Continent.getCountries().iterator();
-
-			while (l_ListIterator.hasNext()) {
-
-				Country l_Country = (Country) l_ListIterator.next();
-				System.out.format(l_tablePattern, l_Country.getD_countryName(), l_Continent.getD_continentName(), l_Country.createNeighboursList(l_Country.getNeighbors()), l_Country.getArmies());
-			}
+		System.out.format("****************************************************************************************************%n");
+		System.out.format("        Country   !     Continent    !     Neighbours                                  %n");
+		System.out.format("****************************************************************************************************%n");
+		for (Map.Entry<Country, List<Country>> entry : getBorders().entrySet()) {
+			String l_tablePattern = "- %-25s - %-20s- %-70s %n";
+			System.out.format(l_tablePattern, entry.getKey().getCountryId(), entry.getKey().getContinent().getContinentId(),
+					getCountriesList(entry.getValue()));
 		}
 
-		System.out.format(
-				"****************************************************************************************************%n");
-
+		System.out.format("****************************************************************************************************%n");
 
 		HashMap<String, Player> l_Players = d_MapModel.getPlayers();
 		d_logger.setLogMessage("\nPlayers : ");
@@ -279,7 +274,7 @@ public class MapModel {
 			d_logger.setLogMessage("");
 		}
 
-		//Showing the Ownership of the players
+		// Showing the Ownership of the players
 		d_logger.setLogMessage("Continents allotted to players: ");
 
 
@@ -289,47 +284,17 @@ public class MapModel {
 
 		String l_Table1 = "- %-15s- %-30s- %n";
 
-
 		for (Player l_Player : d_MapModel.getPlayers().values()) {
-			System.out.format(l_Table1, l_Player.getName(), l_Player.createACaptureList(l_Player.getCapturedCountries()), l_Player.getReinforcementArmies());
+			System.out.format(l_Table1, l_Player.getName(),
+					l_Player.createACaptureList(l_Player.getCapturedCountries()),
+					l_Player.getReinforcementArmies());
 		}
 
 		System.out.format("**************************************************%n");
 
-	}
 
-	public void p_addContinent(String p_ContinentName, String p_ControlValue) throws Exception {
-		if (this.getD_Continents().containsKey(p_ContinentName)) {
-			throw new Exception("Continent already exists");
-		}
-		Continent l_Continent = new Continent();
-		l_Continent.setD_continentName(p_ContinentName);
-		l_Continent.setD_AwardArmies(Integer.parseInt(p_ControlValue));
-		this.getD_Continents().put(p_ContinentName, l_Continent);
-		d_logger.setLogMessage("Continent " + p_ContinentName + " added.");
-	}
 
-	public void p_addCountry(String p_CountryName, String p_ContinentName) throws Exception{
 
-		if (this.getD_Countries().containsKey(p_CountryName)) {
-			throw new Exception("Country already exist");
-		}
-		Country l_Country = new Country();
-		l_Country.setD_countryName(p_CountryName);
-		l_Country.setD_Continent(p_ContinentName);
-		this.getD_Countries().put(p_CountryName, l_Country);
-		this.getContinent(p_ContinentName).getCountries().add(l_Country);
-		d_logger.setLogMessage("Country " + p_CountryName + " added.");
-	}
-
-	public void p_addNeighbor(String p_CountryName, String p_NeighborCountryName) throws Exception {
-		Country l_Country1 = this.getCountry(p_CountryName);
-		Country l_Country2 = this.getCountry(p_NeighborCountryName);
-		if (Objects.isNull(l_Country1) || Objects.isNull(l_Country2)) {
-			throw new Exception("At least one of the mentioned Countries does not exist");
-		}
-		l_Country1.getNeighbors().add(l_Country2);
-		d_logger.setLogMessage(String.format("Route added between : %s - %s", p_CountryName, p_NeighborCountryName));
 	}
 
 
@@ -356,13 +321,15 @@ public class MapModel {
 		int l_Index = 0;
 		List<Player> l_Players = d_MapModel.getPlayers().values().stream().collect(Collectors.toList());
 
-		List<Country> l_ListOfCountries = d_MapModel.getD_Countries().values().stream().collect(Collectors.toList());
+		List<Country> l_ListOfCountries =
+				d_MapModel.getCountries();
+
 		Collections.shuffle(l_ListOfCountries);
 		for (Country l_Country : l_ListOfCountries) {
 			Player l_Player = l_Players.get(l_Index);
 			l_Player.getCapturedCountries().add(l_Country);
 			l_Country.setPlayer(l_Player);
-			d_logger.setLogMessage(l_Country.getD_countryName() + " Allotted to " + l_Player.getName());
+			d_logger.setLogMessage(l_Country.getCountryId() + " Allotted to " + l_Player.getName());
 
 			if (l_Index < d_MapModel.getPlayers().size() - 1) {
 				l_Index++;
@@ -394,8 +361,7 @@ public class MapModel {
 	}
 
 	public void clearMap() {
-		MapModel.getInstance().getD_Continents().clear();
-		MapModel.getInstance().getD_Countries().clear();
+
 		MapModel.getInstance().getPlayers().clear();
 		
 		if( MapModel.getInstance().getContinents() != null) {
