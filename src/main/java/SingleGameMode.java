6f4bdde754c141df214b577bf48c1c5d4d0633ad
Kevin <129557638+SpankeyDaMankey@users.noreply.github.com>
2023-10-04 22:36:06 -0400

    saingle game mode , tornament javadoc

--- src/main/java/SingleGameMode.java
+++ src/main/java/SingleGameMode.java
@@ -1,4 +1,8 @@
-
+/**
+ * Single game mode class to be implemented in the future
+ * @author Kevin
+ * @version build 1
+ */
 public class SingleGameMode {
 
 }
--- src/main/java/TorunamentGameMode.java
+++ src/main/java/TorunamentGameMode.java
@@ -1,4 +1,9 @@
-
+/**
+ * Tournament Game Mode to be implemented in the future
+ * @author Kevin
+ * @version build 1
+ *
+ */
 public class TorunamentGameMode {
 
 }
