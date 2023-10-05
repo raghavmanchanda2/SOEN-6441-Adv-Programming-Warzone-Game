fb7eaf715bb2a6aee9cffb38f0b3a770e55714bf
raghavmanchanda <raghavmanchanda@raghavs-air.wireless.concordia.ca>
2023-10-04 22:49:06 -0400

    Junit Reinforcement Army test

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.player.PlayerStrategy;
+import persistence.MapFileAlteration;
 
 /**
  * Test case to ensure the amount of reinforcement armies are properly calculated and deployed
@@ -20,6 +22,8 @@ import model.player.PlayerStrategy;
  * @version build 1
  */
 class PlayerTest {
+	Player P;
+	MapFileAlteration d_MFA;
 	
 	MapModel MM;
 	
@@ -191,6 +195,40 @@ class PlayerTest {
 		int army = 4;
 		
 		assertFalse(P1.deployReinforcementArmiesFromPlayer(army));
+	}
+	
+	/**
+	 * Test to verify that if all continents are being captured then the bonus armies should be given to player
+	 * The test is performed with the following steps described below
+	 * 1. Create a single continent and multiple countries
+	 * 2. Add those elements to the map
+	 * 3. Call the calculateReinforcementArmies() method
+	 * 4. Compare the the expected reinforcement armies with the value that is defined in the continent value
+	 * 5. Currently only comparing with reinforcement army equals to 3
+	 */
+	@Test
+	void calculateReinforcement() {
+		d_MFA=new MapFileAlteration();
+		Continent l_Asia;
+		
+		Country l_China, l_India, l_Japan, l_Korea;
+		
+		l_Asia = new Continent(2,"Asia");
+		
+		l_China = new Country("China", l_Asia);
+		l_India = new Country("India", l_Asia);
+		l_Japan = new Country("Japan", l_Asia);
+		l_Korea = new Country("Korea", l_Asia);
+		
+		P = new Player(PlayerStrategy.getStrategy("human"));
+		P.setName("Ishan");
+		P.setCapturedCountries(Arrays.asList(l_China,l_India,l_Japan,l_Korea));
+	
+		d_MFA.getMapModel().setD_CurrentPlayer(P);
+		int armyValue;
+		P.calculateReinforcementArmies(d_MFA.getMapModel());
+		armyValue=P.getReinforcementArmies();
+		assertEquals(armyValue,3);
 	}
 
 }
