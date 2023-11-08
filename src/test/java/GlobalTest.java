2b6e000df981b972af9496822bd3e00e743a7e0d
raghavmanchanda2 <raghavmanchanda2@gmail.com>
2023-11-08 16:31:14 -0500

    [Build Issue Fix]

--- src/test/java/GlobalTest.java
+++ src/test/java/GlobalTest.java
@@ -1,6 +1,6 @@
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
+//import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.platform.suite.api.Suite;
+//
 //@Suite
 //@SelectClasses({business.Order.BusinessOrderTestSuite.class})
 public class GlobalTest {
