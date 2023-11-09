1e389431c2e8ba440dead3644afdc32c30d0faf8
raghavmanchanda2 <raghavmanchanda2@gmail.com>
2023-11-08 19:40:01 -0500

    [Added 2 Junit test][add player][remove Player]

--- src/test/java/businessTest/MainPlayPhaseBusinessCommandsTest.java
+++ src/test/java/businessTest/MainPlayPhaseBusinessCommandsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import business.MainPlayPhaseBusinessCommands;
+import business.SingleGamePlayerCommands;
 import logger.GeneralException;
 import model.Continent;
 import model.Country;
@@ -28,11 +29,14 @@ class MainPlayPhaseBusinessCommandsTest {
 	private Continent d_America;
 	
 	private Country d_Canada, d_USA, d_Mexico;
+	
+	private SingleGamePlayerCommands singleGamePlayCommand=new SingleGamePlayerCommands();
 
 	
 	@BeforeEach
 	void setUp(){
 		
+		gameModel.setPlayers(null);
 		d_America = new Continent("North America", "10");
 		
 		d_Canada = new Country("Canada", d_America);
@@ -80,5 +84,34 @@ class MainPlayPhaseBusinessCommandsTest {
 		
 		assertEquals(201, finalResponse.getStatusValue());
 	}
-
+	
+	@Test
+	void addPlayer() throws GeneralException {
+		
+		boolean inPlayerList = false;
+		
+		for(Player player : this.gameModel.getPlayers()) {
+			if(player == P1) {
+				inPlayerList = true;
+			}
+		}
+		
+		assertTrue(inPlayerList);
+	}
+	
+	@Test
+	void removePlayer() throws GeneralException {
+		
+		boolean playerRemoved = false;
+		singleGamePlayCommand.removeplayerFromGame(P1.getPlayerName());
+		if(this.gameModel.getPlayers()==null)
+		{
+			playerRemoved=true;
+		}else if(!this.gameModel.getPlayers().contains(P1))
+		{
+			playerRemoved=true;
+		}
+		assertTrue(playerRemoved);
+		
+	}
 }
