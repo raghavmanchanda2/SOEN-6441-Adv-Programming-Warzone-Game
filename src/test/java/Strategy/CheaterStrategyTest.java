9d82f6a4f1b66ba4970f2972c2e04286754305da
bhavyabugude <bhavyab0610@gmail.com>
2023-11-29 16:26:27 -0500

    Edited CheaterStrategyTest file

--- src/test/java/Strategy/CheaterStrategyTest.java
+++ src/test/java/Strategy/CheaterStrategyTest.java
@@ -29,35 +29,10 @@ class CheaterStrategyTest {
         d_USA = new Country("USA", d_America);
         d_Mexico = new Country("Mexico", d_America);
 
-        // Assuming the setup for neighboring countries and any other setup needed
-        // ...
     }
 
     @Test
     void testGetStrategyName() {
-        assertEquals("CHEATER", d_CheaterStrategy.getStrategyName(), "Strategy name should match 'CHEATER'.");
+        assertEquals("CHEATER", d_CheaterStrategy.getStrategyName());
     }
-
-    // Hypothetical tests for other methods (toAttack, toDefend, etc.) go here
-    // The actual implementations will depend on how these methods are designed to work within the strategy
-
-    // Example test (hypothetical behavior):
-    @Test
-    void testToDefend() {
-        // Assuming toDefend in CheaterStrategy doubles the armies in the weakest country
-        d_Player.addCountryHold(d_Canada);
-        d_Player.addCountryHold(d_USA);
-        d_Player.addCountryHold(d_Mexico);
-        
-        d_Canada.setArmy(10);
-        d_USA.setArmy(5);      // Initially weakest country
-        d_Mexico.setArmy(15);
-
-        Country result = d_CheaterStrategy.toDefend();
-        assertNotNull(result, "toDefend should return a non-null country.");
-        assertEquals(d_USA, result, "toDefend should return the weakest country (USA) to double its armies.");
-        // Additional test: Check if USA's army count is doubled
-    }
-
-    // Similar hypothetical tests for other methods...
 }
