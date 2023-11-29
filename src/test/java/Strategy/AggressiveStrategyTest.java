916ad044ea7111468d30cd4efb9e6b3e734163c7
bhavyabugude <bhavyab0610@gmail.com>
2023-11-29 00:10:40 -0500

    Created Strategy test package with first 3 test files

---
+++ src/test/java/Strategy/AggressiveStrategyTest.java
@@ -0,0 +1,15 @@
+package Strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AggressiveStrategyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
---
+++ src/test/java/Strategy/BenevolentStrategyTest.java
@@ -0,0 +1,14 @@
+package Strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BenevolentStrategyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
