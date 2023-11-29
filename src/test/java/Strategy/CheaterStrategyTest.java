dcc8fc542e8903b260d462c991e422f4cf3ab127
bhavyabugude <bhavyab0610@gmail.com>
2023-11-29 16:07:03 -0500

    Test Case for CheaterStrategyTest file edited

--- src/test/java/Strategy/CheaterStrategyTest.java
+++ src/test/java/Strategy/CheaterStrategyTest.java
@@ -9,7 +9,6 @@ import model.Country;
 import model.Continent;
 import controller.MainPlayPhaseController;
 import business.MainPlayPhaseBusinessCommands;
-import logger.GeneralException;
 import model.ResponseWrapper;
 
 class CheaterStrategyTest {
@@ -17,18 +16,21 @@ class CheaterStrategyTest {
     private CheaterStrategy d_CheaterStrategy;
     private Player d_Player;
     private MapModel d_MapModel;
-    private Continent d_Continent;
-    private Country d_Country1, d_Country2;
+    private Continent d_America;
+    private Country d_Canada, d_USA, d_Mexico;
 
     @BeforeEach
     void setUp() {
-        d_Player = new Player("PlayerName");
+        d_Player = new Player("Kevin");
         d_MapModel = new MapModel();
         d_CheaterStrategy = new CheaterStrategy(d_Player, d_MapModel, null, null);
-        d_Continent = new Continent("Continent");
-        d_Country1 = new Country("Country1", d_Continent);
-        d_Country2 = new Country("Country2", d_Continent);
-        // Further setup for countries and continents
+        d_America = new Continent("North America");
+        d_Canada = new Country("Canada", d_America);
+        d_USA = new Country("USA", d_America);
+        d_Mexico = new Country("Mexico", d_America);
+
+        // Assuming the setup for neighboring countries and any other setup needed
+        // ...
     }
 
     @Test
@@ -36,4 +38,26 @@ class CheaterStrategyTest {
         assertEquals("CHEATER", d_CheaterStrategy.getStrategyName(), "Strategy name should match 'CHEATER'.");
     }
 
+    // Hypothetical tests for other methods (toAttack, toDefend, etc.) go here
+    // The actual implementations will depend on how these methods are designed to work within the strategy
+
+    // Example test (hypothetical behavior):
+    @Test
+    void testToDefend() {
+        // Assuming toDefend in CheaterStrategy doubles the armies in the weakest country
+        d_Player.addCountryHold(d_Canada);
+        d_Player.addCountryHold(d_USA);
+        d_Player.addCountryHold(d_Mexico);
+        
+        d_Canada.setArmy(10);
+        d_USA.setArmy(5);      // Initially weakest country
+        d_Mexico.setArmy(15);
+
+        Country result = d_CheaterStrategy.toDefend();
+        assertNotNull(result, "toDefend should return a non-null country.");
+        assertEquals(d_USA, result, "toDefend should return the weakest country (USA) to double its armies.");
+        // Additional test: Check if USA's army count is doubled
+    }
+
+    // Similar hypothetical tests for other methods...
 }
